5320cd081df1fb7059d7cd41cd913d0a695cc5d3
alpbourne <alpbourne@gmail.com>
2017-09-29 22:30:43 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,7 +14,7 @@ class Song
     song_name = (filename.split(" - ")[1])
     artist = (filename.split(" - ")[0])
     song = self.new(song_name)
-    song.artist_name = artist
+    self.artist = Artist.find_or_create_by_name(name)
     song
   end
 
